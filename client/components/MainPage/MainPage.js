3465cc47667a2dcc7a8c57033b3787e23d5559c5
Vitaly <avers3452@gmail.com>
2020-11-30 20:32:26 -0500

    test to connect

--- client/components/MainPage/MainPage.js
+++ client/components/MainPage/MainPage.js
@@ -2,27 +2,8 @@ import React from 'react';
 
 const MainPage = (props) => {
     return (
-        <div>
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
-            It is MainPage It is MainPage It is MainPage 
+        <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente voluptate molestiae sint cumque quidem explicabo aliquid laboriosam amet ipsa voluptatum, tempora, voluptas et temporibus tenetur nisi dolore autem fugiat asperiores?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam nam quidem, deleniti assumenda velit officiis, hic placeat, optio tenetur at voluptatibus. Alias, totam nostrum porro qui eveniet perspiciatis dolor distinctio?
         </div>
     )
 }
