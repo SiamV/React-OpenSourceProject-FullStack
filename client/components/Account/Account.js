de226d8be362fef2cbfbbb8fecde23edc379c298
Vitaly <avers3452@gmail.com>
2021-01-12 11:38:34 -0500

    add button admin in account component

--- client/components/Account/Account.js
+++ client/components/Account/Account.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import {NavLink} from 'react-router-dom';
+import {useSelector, useDispatch} from 'react-redux';
 
-import { logOutAC } from '../../redux/reducers/loginReducer'
+import {logOutAC} from '../../redux/reducers/loginReducer'
 import classes from './Account.module.css'
 
 const Account = () => {
@@ -11,7 +11,14 @@ const Account = () => {
     return (
         <div className={classes.accountWrapper}>
             {!isAuth && <NavLink to={'/login'}>Login</NavLink>}
-            {isAuth && <div onClick={() => {dispatch(logOutAC())}}>LogOut</div>}
+            {isAuth &&
+            <div>
+                <button onClick={() => {
+                    dispatch(logOutAC())
+                }}>LogOut
+                </button>
+                <button><NavLink to={'/admin'}>admin</NavLink></button>
+            </div>}
         </div>
     )
 }
