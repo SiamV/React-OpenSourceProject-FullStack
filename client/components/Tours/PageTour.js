a4f1af350cdc3af1128621689ccaba654d88b222
Vitaly <avers3452@gmail.com>
2021-01-20 22:14:50 -0500

    in local state EditorState

--- client/components/Tours/PageTour.js
+++ client/components/Tours/PageTour.js
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 import {useParams} from 'react-router-dom'
 import {useDispatch, useSelector} from "react-redux";
 import {setToursThunkCreator} from "../../redux/reducers/toursReducer";
-import {Editor, EditorState, RichUtils, getDefaultKeyBinding, convertToRaw, convertFromRaw} from 'draft-js';
+import {Editor, EditorState, convertFromRaw} from 'draft-js';
 
 const PageTour = () => {
     const {idTour} = useParams();
@@ -13,17 +13,23 @@ const PageTour = () => {
         dispatch(setToursThunkCreator())
     }, [])
 
-    const DB = convertFromRaw(JSON.parse(tours.map((t, index) => (t.tour))))
-    const [editorState, setEditorState] = React.useState(() => EditorState.createWithContent(
-        DB));
+    // const DB = convertFromRaw(JSON.parse(tours.map((t, index) => (t.tour))))
+    const [editorState, setEditorState] = React.useState(tours);
     console.log(editorState)
+    console.log(tours)
+    useEffect(() => {
+        setEditorState(editorState.map((t, index) =>
+            ({...t, tour: EditorState.createWithContent(convertFromRaw(JSON.parse(t.tour)))})
+        ))}, [])
+
+    //content: EditorState.createWithContent(convertFromRaw(JSON.parse(t.tour)))
 
     return (
         <div>
-        <div>{tours.filter(t => t._id === idTour).map((t, index) => (
+        <div>{editorState.filter(t => t._id === idTour).map((t, index) => (
             <div key={t._id}>
                 <div>{t.tourTitle}</div>
-                <div>{t.tour}</div>
+                {/*<Editor readOnly={true} editorState={t.tour.getCurrentContent()} />*/}
             </div>
         ))}
         </div>
