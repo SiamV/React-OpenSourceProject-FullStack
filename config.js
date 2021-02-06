0d11fdc11bbcee9e21f4230f4b231fc864934c4b
Vitaly <avers3452@gmail.com>
2021-02-05 20:01:48 -0500

    connect real mogoDB

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 const config = {
     port: 8090, //server port
-    url: 'mongodb://localhost:27017/site1', //use your url to connect real DB
+    url: 'mongodb+srv://adminDB:GJ7HncFNIHNkN6yQ@test1.wwbas.mongodb.net/test1', //use your url to connect real DB
     secret: 'secret' //secret word for hash
   }
   
   export default config;
+
+// mongodb://localhost:27017/site1
